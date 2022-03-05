d768ea64a648abef778267a2b81a174615a78911
Cole Martin <colemar@pop-os.localdomain>
2022-03-05 11:02:48 -0800

    cnn classifier, want regression

---
+++ image_from_directory.py
@@ -0,0 +1,69 @@
+import numpy as np
+import os
+import PIL
+import PIL.Image
+import tensorflow as tf
+
+
+print(tf.__version__)
+
+
+img_height = 900
+img_width = 900
+
+data_dir = ('/home/colemar/Pictures/A')
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=53,
+  image_size=(img_height, img_width))
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=53,
+  image_size=(img_height, img_width))
+
+class_names = train_ds.class_names
+print(class_names)
+
+normalization_layer = tf.keras.layers.Rescaling(1./255)
+
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+num_classes = 6
+
+model = tf.keras.Sequential([
+  tf.keras.layers.Rescaling(1./255),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(num_classes)
+])
+
+model.compile(
+  optimizer='adam',
+  loss=tf.keras.losses.MeanSquaredError())
+
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=10
+)
+print(history.history.keys())
+print("final MSE for train is %.2f and for validation is %.2f" % 
+      (history.history['loss'][-1], history.history['val_loss'][-1]))
