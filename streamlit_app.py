d737e0e87508a92d30dbef069333a138e1e520c3
Luke Orr <leorr@uw.edu>
2022-03-08 12:53:48 -0800

    Model additon funciton

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 import os
+
+@st.cache()
+def load_model
+	"""Retrieves the model for the test"""
+	model = tf.keras.models.load_model('classifier BC')
+	return model
+
 st.title('QIAML')
 st.header('Upload Test File Here')
 file_bytes = st.file_uploader("Upload a file", type=("png", "jpg", "bmp"))
