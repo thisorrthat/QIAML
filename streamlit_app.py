dedc81baf268c785ac71e13cdb3862828b7e4aa1
Luke Orr <leorr@uw.edu>
2022-03-08 14:23:57 -0800

    More development of app

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,21 @@ def load_model
 	"""Retrieves the model for the test"""
 	model = tf.keras.models.load_model('classifier BC')
 	return model
+@st.cache()
+def predict(
+	img: 
+
 
 st.title('QIAML')
 st.header('Upload Test File Here')
-file_bytes = st.file_uploader("Upload a file", type=("png", "jpg", "bmp"))
+file = st.file_uploader("Upload a image", type=("png", "jpg", "bmp"))
+if file: #is uploaded file
+	img = Image.open(file)
+	prediction = predict(img, model)
+	top_prediciton = prediciton
+
+st.title("Image Selected")
+resized_image = img.resize((512, 512))
+st.image(resized_image)
+st.title("Predicted Result")
+
