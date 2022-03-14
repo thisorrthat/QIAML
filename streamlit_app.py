40ec69a9a25a84b5d160b1689d89be3a632a4b64
Luke Orr <leorr@uw.edu>
2022-03-14 14:33:40 -0700

    More app dev

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,25 @@
 import streamlit as st
 from PIL import Image
+
+import tensorflow as tf
 import numpy as np
 
-Results = "HIGH"
-st.title("QIAML Test Results")
+from model import loadmodel
 
+
+st.title("QIAML Test Results")
 
 file = st.file_uploader("Upload the image that you are wanting to classify here, either browse or drag ", type=("jpg"))
 
 
+
+
 if file: 
     img = Image.open(file)
-    predict = np.asarray(img)
     st.title("Image Selected")
     resized_image = img.resize((200, 200))
     st.image(resized_image)
     st.title("Predicted Result")
-    st.write(Results)
+    predictions = loadmodel(file)
+    st.write(predictions)
+
