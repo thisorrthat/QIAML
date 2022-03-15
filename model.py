71620caf0324ee40ee030b06ebb5d454361e5e1b
Luke Orr <leorr@uw.edu>
2022-03-14 19:10:34 -0700

    Working classification APP!!!

--- model.py
+++ model.py
@@ -4,12 +4,15 @@ import numpy as np
 
 """This will load the model and return model converted into probability resutls
 """
-def loadmodel(img):
+def loadmodel():
     
     model = tf.keras.models.load_model('classmodel')
-    probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+    model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+    return model
+   
+def predict(img, model):
     img_resize = img.copy()
     img_resized = img_resize.resize((900,900))
     img_resized = (np.expand_dims(img_resized,0))
-    predictions = probability_model.predict(img_resized)
+    predictions = model.predict(img_resized)
     return predictions
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,23 @@
 import streamlit as st
 from PIL import Image
-
-from model import loadmodel
+import numpy as np
+from model import loadmodel, predict
 
 
 st.title("QIAML Test Results")
 
 file = st.file_uploader("Upload the image that you are wanting to classify here, either browse or drag ", type=("jpg"))
+model = loadmodel()
 
-
+class_names = ['High', 'Low', 'Medium']
 if file: 
     img = Image.open(file)
     st.title("Image Selected")
     resized_image = img.resize((200, 200))
     st.image(resized_image)
-    predictions = loadmodel(img)
+    predictions = predict(img, model)
+    predictions_label = np.argmax(predictions[0])
+    predictions_label = class_names[predictions_label]
     st.title("Predicted Result")
-    st.write(predictions)
+    st.title(predictions_label)
 
